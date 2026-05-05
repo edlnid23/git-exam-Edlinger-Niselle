299b5a14343186936957547248906b887becd172
edlnid23 <edlnid23@htl-kaindorf.at>
2026-05-05 11:16:00 +0200

    Developer Data added

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
-public class App(){
+class App{
     public static void main(String[] args){
         System.out.println("System Initialized.");
+	System.out.println("Developer: Edlinger Niselle");
     }
 }
